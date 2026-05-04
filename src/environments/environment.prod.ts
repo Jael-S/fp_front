98c1d91516d73556a8ea39921c432519d7de9a4d
Jael-S <sandovaljael08@gmail.com>
2026-05-04 05:58:36 -0400

    Actuañizacion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://52.15.220.152:8080/api/v1',
-  wsUrl: 'http://52.15.220.152:8080/ws',
-  wsMonitorUrl: 'http://52.15.220.152:8080/ws-monitor',
+  apiUrl: '/api/v1',           // ← relativo, Nginx resolverá
+  wsUrl: '/ws',
+  wsMonitorUrl: '/ws-monitor'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api/v1',
-  wsUrl: 'http://localhost:8080/ws',
-  wsMonitorUrl: 'http://localhost:8080/ws-monitor',
+  production: false,   // aunque sea false, no importa
+  apiUrl: '/api/v1',
+  wsUrl: '/ws',
+  wsMonitorUrl: '/ws-monitor'
 };
